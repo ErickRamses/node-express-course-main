dbba99dfa55951a499d42ed9626e98a9e7569b6a
ErickRamses <105986183+ErickRamses@users.noreply.github.com>
2022-08-18 23:02:48 -0700

    node ta

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+
+// const http =require("http")
+// const { endianness } = require("os")
+
+// const bunny = http.createServer((req,res)=>{
+    
+//     console.log('Express Tutorial')
+//     console.log('does it work?? of course that it will jajajsasjsajsajsajjdjsajdjasjdjsajdsakjfhd')
+//     res.end("this is not in irland js")
+// })
+// bunny.listen(5000)
+const express =require("express")
+const app=express()
+
+app.listen(5000,()=>{
+    console.log("serverlistening")
+})
